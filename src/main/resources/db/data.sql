485943047f4355f59a2f5b5490d9002124679b43
Kjuk <juk6340@gmail.com>
2023-02-15 10:35:00 +0900

    프로젝트 생성 및 세팅 완료

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_tb(username, password, email, created_at) values('ssar', '1234', 'ssar@nate.com', now());
+INSERT INTO user_tb(username, password, email, created_at) values('love', '1234', 'love@nate.com', now());
+
+
+
+commit;
